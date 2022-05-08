67fce0c25b287e2f9cabe47dc4a32ab1833c562a
ro <kronew6@gmail.com>
2022-05-08 18:40:27 -0500

    adds file in proper service package

--- src/main/java/com/rookim/todoapp/TodoService.java
+++
@@ -1,5 +0,0 @@
-package com.rookim.todoapp;
-
-public class TodoService {
-
-}
---
+++ src/main/java/com/rookim/todoapp/service/TodoService.java
@@ -0,0 +1,15 @@
+package com.rookim.todoapp.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.rookim.todoapp.domain.TodoItem;
+
+@Service
+public class TodoService {
+	
+	public List<TodoItem> fetchAllTodoItems () {
+		
+	}
+}
