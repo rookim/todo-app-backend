9dee4b6324bc5c6f23b4cf19f9f093f48f2f62b5
ro <kronew6@gmail.com>
2022-05-08 19:04:17 -0500

    adds necessary dependencies

--- src/main/java/com/rookim/todoapp/service/TodoService.java
+++ src/main/java/com/rookim/todoapp/service/TodoService.java
@@ -2,14 +2,18 @@ package com.rookim.todoapp.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rookim.todoapp.domain.TodoItem;
+import com.rookim.todoapp.repository.TodoRepository;
 
 @Service
 public class TodoService {
 	
+	@Autowired // inject
+	private TodoRepository todoRepo; 
 	public List<TodoItem> fetchAllTodoItems () {
-		
+		return todoRepo.fetchAllTodoItems();
 	}
 }
--- src/main/java/com/rookim/todoapp/web/TodoController.java
+++ src/main/java/com/rookim/todoapp/web/TodoController.java
@@ -1,5 +1,32 @@
 package com.rookim.todoapp.web;
 
-public class TodoController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rookim.todoapp.domain.TodoItem;
+import com.rookim.todoapp.service.TodoService;
+
+// annotation (like syntactical sugar)
+// command + shift + O --> import automatically after @RestController was typed out
+// rest controller returns JSON response
+// regular controller returns a view?
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class TodoController {
+	
+	@Autowired
+	private TodoService todoService; 
+	
+//	fetch all todo items from database
+	@GetMapping("/api/todoItems")
+	public ResponseEntity<?> fetchAllTodoItems () {
+		List<TodoItem> todoItems = todoService.fetchAllTodoItems();
+		
+		return ResponseEntity.ok(todoItems);
+	}
 }
